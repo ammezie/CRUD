2970db0f1b45a187d5220456c18778979108d7b2
Mezie <meziemichael@gmail.com>
2015-03-09 15:05:50 +0100

    First commit to the CRUD application

---
+++ db/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+//collect to our database
+
+$db_server 	= "localhost";
+$db_user	= "root";
+$db_pass 	= "";
+$db_name	= "crud";
+
+$db = new mysqli($db_server, $db_user, $db_pass, $db_name);
+
+if($db->connect_errno){
+	echo "Could not connect to database";
+}
